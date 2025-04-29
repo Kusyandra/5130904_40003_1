7b1c92f734ec4232e0f4d3c4cc0f6ea3574cf191
Kusyandra <lixach3210@mail.ru>
2025-04-30 02:35:30 +0300

    Create main.cpp

---
+++ gorbunov.pavel/T2/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include "DataStruct.h"
+
+void cleanVector(std::vector<DataStruct>& vec)
+{
+    vec.erase(
+        std::remove_if(vec.begin(), vec.end(),
+            [](const DataStruct& ds) {
+                return ds.key1 == INVALID_DOUBLE_INDICATOR &&
+                    ds.key2 == INVALID_LL_INDICATOR &&
+                    ds.key3 == INVALID_STR_INDICATOR;
+            }),
+        vec.end());
+}
+
+int main()
+{
+    std::vector<DataStruct> data;
+
+    std::copy(
+        std::istream_iterator<DataStruct>(std::cin),
+        std::istream_iterator<DataStruct>(),
+        std::back_inserter(data)
+    );
+
+    cleanVector(data);
+
+    if (data.empty())
+    {
+        std::cerr << "Warning: No valid data structures found\n";
+        return 0;
+    }
+
+    std::sort(data.begin(), data.end(), compareDataStruct);
+
+    std::copy(
+        data.begin(),
+        data.end(),
+        std::ostream_iterator<DataStruct>(std::cout, "\n")
+    );
+
+    return 0;
+}
