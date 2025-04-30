a3bec0da425896095433b27f7916cae343431cac
Kusyandra <lixach3210@mail.ru>
2025-04-30 12:21:27 +0300

    Update data_struct.hpp

--- gorbunov.pavel/T2/data_struct.hpp
+++ gorbunov.pavel/T2/data_struct.hpp
@@ -1,57 +1,57 @@
-#ifndef CUSTOM_STRUCT_HPP
-#define CUSTOM_STRUCT_HPP
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
 
 #include <iostream>
 #include <string>
 #include <iomanip>
 
-namespace custom_namespace
+namespace petrov
 {
-  struct CustomData
+  struct DataStruct
   {
-    double firstValue;
-    long long secondValue;
-    std::string thirdValue;
+    double key1;
+    long long key2;
+    std::string key3;
   };
 
-  struct TokenSeparator
+  struct DelimiterIO
   {
-    char delimiter;
+    char exp;
   };
 
-  struct DoubleInput
+  struct DoubleIO
   {
-    double & value;
+    double & ref;
   };
 
-  struct LongLongInput
+  struct LongLongIO
   {
-    long long & value;
+    long long & ref;
   };
 
-  struct StringInput
+  struct StringIO
   {
-    std::string & value;
+    std::string & ref;
   };
 
-  class StreamManager
+  struct StreamGuard
   {
   public:
-    explicit StreamManager(std::basic_ios< char > & stream);
-    ~StreamManager();
+    explicit StreamGuard(std::basic_ios< char > & s);
+    ~StreamGuard();
   private:
-    std::basic_ios< char > & stream_;
+    std::basic_ios< char > & s_;
     std::streamsize width_;
     char fill_;
     std::streamsize precision_;
-    std::basic_ios< char >::fmtflags format_;
+    std::basic_ios< char >::fmtflags fmt_;
   };
 
-  std::istream & operator>>(std::istream & in, TokenSeparator && token);
-  std::istream & operator>>(std::istream & in, DoubleInput && input);
-  std::istream & operator>>(std::istream & in, StringInput && input);
-  std::istream & operator>>(std::istream & in, CustomData & data);
-  std::ostream & operator<<(std::ostream & out, const CustomData & data);
+  std::istream & operator>>(std::istream & in, DelimiterIO && dest);
+  std::istream & operator>>(std::istream & in, DoubleIO && dest);
+  std::istream & operator>>(std::istream & in, StringIO && dest);
+  std::istream & operator>>(std::istream & in, DataStruct & dest);
+  std::ostream & operator<<(std::ostream & out, const DataStruct & dest);
 }
 
 #endif
