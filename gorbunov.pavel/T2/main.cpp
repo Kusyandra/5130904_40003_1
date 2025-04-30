b47806873076702daba22d48945289643d316fbe
Kusyandra <lixach3210@mail.ru>
2025-04-30 11:51:12 +0300

    Update main.cpp

--- gorbunov.pavel/T2/main.cpp
+++ gorbunov.pavel/T2/main.cpp
@@ -1,46 +1,52 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <iterator>
-#include "DataStruct.h"
+#include <functional>
+#include <iostream>
+#include "data_structure.hpp"
 
-void cleanVector(std::vector<DataStruct>& vec)
+namespace
 {
-    vec.erase(
-        std::remove_if(vec.begin(), vec.end(),
-            [](const DataStruct& ds) {
-                return ds.key1 == INVALID_DOUBLE_INDICATOR &&
-                    ds.key2 == INVALID_LL_INDICATOR &&
-                    ds.key3 == INVALID_STR_INDICATOR;
-            }),
-        vec.end());
+  bool compareContainers(const custom_namespace::DataContainer & lhs, const custom_namespace::DataContainer & rhs);
+  bool compareContainers(const custom_namespace::DataContainer & lhs, const custom_namespace::DataContainer & rhs)
+  {
+    if (lhs.firstKey < rhs.secondKey)
+    {
+      return true;
+    }
+    else if (lhs.secondKey < rhs.secondKey)
+    {
+      return true;
+    }
+    else if (lhs.thirdKey.length() < rhs.thirdKey.length())
+    {
+      return true;
+    }
+    return false;
+  }
 }
 
 int main()
 {
-    std::vector<DataStruct> data;
-
+  using namespace custom_namespace;
+  std::vector<DataContainer> data;
+  while (!std::cin.eof())
+  {
+    if (!std::cin)
+    {
+      std::cin.clear();
+      std::cin.ignore(1024, '\n');
+    }
     std::copy(
-        std::istream_iterator<DataStruct>(std::cin),
-        std::istream_iterator<DataStruct>(),
-        std::back_inserter(data)
+      std::istream_iterator<DataContainer>(std::cin),
+      std::istream_iterator<DataContainer>(),
+      std::back_inserter(data)
     );
-
-    cleanVector(data);
-
-if (data.empty()) {
-    std::cerr << "Warning: No valid data structures found\n";
-    std::cerr << "Looks like there is no supported record. Cannot determine input. Test skipped\n";
-    return 1;
-}
-
-    std::sort(data.begin(), data.end(), compareDataStruct);
-
-    std::copy(
-        data.begin(),
-        data.end(),
-        std::ostream_iterator<DataStruct>(std::cout, "\n")
-    );
-
-    return 0;
+  }
+  std::sort(data.begin(), data.end(), compareContainers);
+  std::copy(
+      std::begin(data),
+      std::end(data),
+      std::ostream_iterator<DataContainer>(std::cout, "\n")
+  );
 }
